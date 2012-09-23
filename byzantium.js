6afccc5bd23da871400f6a2941cca10f2093483f
Sebastian Rahtz <sebastian.rahtz@gmail.com>
2012-09-23 18:05:15 +0100

    tweak display

--- byzantium.js
+++ byzantium.js
@@ -1,6 +1,7 @@
 var url='http://tei.oucs.ox.ac.uk/test.js';
 var url="http://localhost/Byzantium/p5subset.json";
 var TEI=[];
+
 function teijs(data) {
     TEI=data;
     showmodules();
@@ -11,7 +12,9 @@ function showmodules() {
     $.each(TEI.modules, function(i, module) {
 	    items.push('<li><button class="modulelink" style="border:none; color:blue; cursor: pointer;">' + module.ident + '</button>' + module.desc + '</li>');
 	});
-    $('#modules').html($('<ul/>', {
+    $('#modules').html($('<p/>', { html: "Found " + TEI.modules.length + " modules"}));
+
+    $('#modules').append($('<ul/>', {
 	'class': 'modules',
 	html: items.join('')
     }));
@@ -21,21 +24,23 @@ function showmodules() {
 function showelements(name  )
 {
     var items = [];
+    $('#elements').html($('<h2/>', {html: "Elements in module " + name }));
     $.each(TEI.elements, function(i, element) {
 	if (element.module == name) {
 	    items.push('<li><button class="elementlink" style="border:none; color:blue; cursor: pointer;">' + element.ident + '</button>' + element.desc + '</li>');
 	  }
 	});
-    $('#elements').html($('<ul/>', {'class': 'elements',html: items.join('') }));
+    $('#elements').append($('<ul/>', {'class': 'elements',html: items.join('') }));
 }
 
- $(document).ready(function(){ 
-     $('#modules').html($('<p id="message">Loading TEI P5.....</p>', {}));
+$(document).ready(function(){ 
+   $('#message').html('<p>Loading source.....</p>');
    $.ajax({
     url: url,
     dataType: 'jsonp',
     jsonpCallback: 'teijs'
    });
+   $('#message').html('<p>Successfuly read ' + url);
 
 });
 $(document).on("click","button.modulelink",function() {
